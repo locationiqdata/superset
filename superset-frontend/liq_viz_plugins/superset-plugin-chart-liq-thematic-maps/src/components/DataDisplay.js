2a737b14b70b0a560672bc06d2b76514d9c4f79b
TusharVirk <Tushar.Virk@locationiq.com.au>
2023-05-09 14:42:45 +1000

    changed data display to transposed

--- superset-frontend/liq_viz_plugins/superset-plugin-chart-liq-thematic-maps/src/components/DataDisplay.js
+++ superset-frontend/liq_viz_plugins/superset-plugin-chart-liq-thematic-maps/src/components/DataDisplay.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Divider, List, Typography, Table } from 'antd';
+import { Descriptions } from 'antd';
 
 export default function DataDisplay(props) {
   const {
@@ -28,13 +29,30 @@ export default function DataDisplay(props) {
       });
       nD.push(row);
     });
+    console.log(nD);
     setNewData([...nD]);
     setIsEmpty(data.length === 0);
   }, [data])
 
   return (
     <>
-      {isEmpty ? 'No Data' : <Table columns={columns} dataSource={newData} /> }
+      {/*isEmpty ? 'No Data' : <Table columns={columns} dataSource={newData} />*/}
+      {isEmpty ? 'No Data' : newData.map((d, i) => (
+        <>
+          <Descriptions 
+            title={'tenantname' in d ? d['tenantname'] : 'name' in d ? d['name'] : ''}
+            layout='horizontal'
+            key={i}
+            bordered
+            column={1}
+          >
+            {Object.keys(d).map(o => !(o === 'key' || o === 'zoom') && (
+              <Descriptions.Item label={o}>{d[o]}</Descriptions.Item>
+            ))}
+          </Descriptions>
+          <Divider />
+        </>
+      ))}
     </>
   )
 }
