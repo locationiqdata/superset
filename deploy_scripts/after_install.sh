ba24df58be99d9eb29310d4e006da1d144543244
TusharVirk <109781601+TusharVirk@users.noreply.github.com>
2023-06-07 08:46:47 +1000

    Update after_install.sh
    Commented in solution for needing to manually change docker-frontend.sh back to production settings

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -6,8 +6,10 @@ sudo rm -f /home/ec2-user/liq_superset/docker/pythonpath_dev/superset_config.py
 sudo rm -f /home/ec2-user/liq_superset/docker/.env
 sudo rm -f /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/liq_secrets.js
 sudo rm -rf /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/liq_data
+# sudo rm -f /home/ec2-user/liq_superset/docker/docker-frontend.sh - Remove commited docker-frontend.sh
 sudo cp /home/ec2-user/superset_config.py /home/ec2-user/liq_superset/docker/pythonpath_dev/
 sudo cp /home/ec2-user/.env /home/ec2-user/liq_superset/docker/
 sudo cp /home/ec2-user/liq_secrets.js /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/
 sudo cp -R /home/ec2-user/liq_data /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/
+# sudo cp /home/ec2-user/docker-frontend.sh /home/ec2-user/liq_superset/docker/ - Replace with docker-frontend.sh production version
 sleep 5
